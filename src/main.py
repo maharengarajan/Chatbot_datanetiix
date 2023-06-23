c2bda872eddb862cfe8f7052f0df3c092366562b
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-06-23 14:58:10 +0530

    'logic_weather_enabled'

--- src/main.py
+++ src/main.py
@@ -1,37 +1,71 @@
+import requests
 import re
 from datetime import datetime
-from database import create_database, insert_new_client, insert_existing_client, insert_job_seeker
 
-create_database()
+def get_ip_address():
+    response = requests.get('https://api.ipify.org').text
+    return response
 
+def get_location():
+    ip_address = get_ip_address()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = response.get("city") 
+    return location_data
+
+def get_weather(city):
+    api_key = "01e6399113b4c255c497958efccc0dc9"  # Weather API key
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url).json()
+
+    if response['cod'] == 200:
+        weather_desc = response['weather'][0]['main']
+        return weather_desc
+    else:
+        return "Sorry, I couldn't retrieve the weather information."
+
+def get_weather_greeting(weather_desc):
+    if weather_desc in ['Thunderstorm', 'Drizzle', 'Rain', 'Snow']:
+        return "It seems like there's {} outside. Stay safe!".format(weather_desc)
+    elif weather_desc in ['Atmosphere', 'Clear', 'Clouds']:
+        return "Enjoy the {} weather!".format(weather_desc)
+    elif weather_desc in ['Mist', 'Smoke', 'Haze', 'Dust', 'Fog', 'Sand', 'Ash']:
+        return "Be cautious as there's {} in the air.".format(weather_desc)
+    elif weather_desc in ['Squall', 'Tornado']:
+        return "Take extra precautions due to {} in the area.".format(weather_desc)
+    else:
+        return "Enjoy your time!"
+    
 def get_valid_name():
-    name = input("Please enter your name: ")
-    while not re.match(r"^[A-Za-z\s]+$", name):
-        print("Please enter a valid name")
-        name = input("Please enter your name: ")
-    return name
-
+    while True:
+        name = input('Please enter your name: ')
+        if re.match(r'^[A-Za-z\s]+$', name):
+            return name
+        else:
+            print("Please enter a valid name")
+            
 def get_valid_email():
-    email = input("Please enter your email address: ")
-    while not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        print("Please enter a valid email address")
-        email = input("Please enter your email address: ")
-    return email
-
-def get_valid_contact():
-    contact = input("Please enter your contact number: ")
-    while not contact.isdigit():
-        print("Please enter a valid contact number")
-        contact = input("Please enter your contact number: ")
-    return contact
-
+    while True:
+        email = input('Please enter your email: ')
+        if re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", email):
+            return email
+        else:
+            print('Please enter a valid email address')
+            
+def get_valid_contact_number():
+    while True:
+        contact = input('Please enter your contact number: ')
+        if re.match(r'^\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$', contact):
+            return contact
+        else:
+            print('Please enter your contact number')
+            
 def get_user_details():
     name = get_valid_name()
     email = get_valid_email()
-    contact = get_valid_contact()
+    contact = get_valid_contact_number()
     return name, email, contact
 
-def choose_industry_options():
+def choose_industries_option():
     industries = {
         '1': 'Insurance',
         '2': 'Banking',
@@ -40,121 +74,104 @@ def choose_industry_options():
         '5': 'Healthcare',
         '6': 'Internet',
         '7': 'Automobile',
-        '8': 'Others'
+        '8': 'Others'  
     }
     valid_options = industries.keys()
     while True:
-        print("Please choose an industry option:")
+        print('Please choose an industry option: ')
         for option, industry in industries.items():
-            print(f"{option}. {industry}")
+            print(f"{option}.{industry}")
         option = input("Enter your option(s) separated by commas: ")
         options = [opt.strip() for opt in option.split(',') if opt.strip() in valid_options]
         if options:
             return [industries[opt] for opt in options]
         else:
             print("Please choose valid option(s)")
-
+            
 def choose_vertical_option():
-    verticals = ['ML/DS/AI', 'Sales force', 'Microsoft dynamics', 'Custom app', 'Others']
-    print("Please choose a vertical option:")
-    for i, vertical in enumerate(verticals, start=1):
-        print(f"{i}. {vertical}")
-
+    verticals = {
+        '1': 'ML/DS/AI',
+        '2': 'Sales force',
+        '3': 'Microsoft dynamics',
+        '4': 'Custom app',
+        '5': 'Others'
+    }
+    valid_options = verticals.keys()
     while True:
+        print("Please choose a vertical option:")
+        for option, vertical in verticals.items():
+            print(f"{option}. {vertical}")
         option = input("Enter your option(s) separated by commas: ")
-        options = option.split(',')
-        selected_verticals = []
-        invalid_options = []
-
-        for opt in options:
-            if opt.strip() not in ['1', '2', '3', '4', '5']:
-                invalid_options.append(opt)
-            else:
-                selected_verticals.append(verticals[int(opt.strip()) - 1])
-
-        if len(invalid_options) == 0:
-            break
+        options = [opt.strip() for opt in option.split(',') if opt.strip() in valid_options]
+        if options:
+            return [verticals[opt] for opt in options]
         else:
-            print("Invalid option(s):", ", ".join(invalid_options))
-
-    print("Thank you for choosing your vertical option(s).")
-    print("Selected vertical(s):", ", ".join(selected_verticals))
-    return selected_verticals
-
-def choose_requirements():
-    requirements = ["Start the project from scratch", "Require support from existing project", "Looking for some kind of solutions", "Others"]
-    print("Please choose your requirements:")
-    for i, requirement in enumerate(requirements, start=1):
-        print(f"{i}. {requirement}")
-
+            print("Please choose valid option(s)")
+            
+def choose_requirement():
+    requirements = {
+        '1': 'Start the project from scratch',
+        '2': 'Require support from existing project',
+        '3': 'Looking for some kind of solutions',
+        '4': 'Others'
+    }
+    
     while True:
-        option = input("Enter your option(s) separated by commas: ")
-        options = option.split(',')
-        selected_requirements = []
-        invalid_options = []
-
-        for opt in options:
-            if opt.strip() not in ['1', '2', '3', '4']:
-                invalid_options.append(opt)
-            else:
-                selected_requirements.append(requirements[int(opt.strip()) - 1])
-
-        if len(invalid_options) == 0:
-            break
+        print("Please choose your requirement:")
+        for option, requirement in requirements.items():
+            print(f"{option}. {requirement}")
+            
+        option = input("Enter your option: ")
+        
+        if option.strip() in requirements:
+            return requirements[option.strip()]
         else:
-            print("Invalid option(s):", ", ".join(invalid_options))
-
-    print("Thank you for choosing your requirement(s).")
-    print("Selected requirement(s):", ", ".join(selected_requirements))
-    return selected_requirements
-
+            print("Please choose a valid option.")
+            
 def choose_known_source():
-    known_sources = [
-        "Google",
-        "LinkedIn",
-        "Email Campaign",
-        "Known resources",
-        "Others"
-    ]
-
-    print("How did you hear about us?")
-    for i, source in enumerate(known_sources, start=1):
-        print(f"{i}. {source}")
+    known_sources = {
+        '1': 'Google',
+        '2': 'LinkedIn',
+        '3': 'Email Campaign',
+        '4': 'Known resources',
+        '5': 'Others'
+    }
 
     while True:
-        option = input("Choose an option (1-5): ")
-        if option.isdigit() and 1 <= int(option) <= 5:
-            option = int(option)
-            if option == 4:
-                specification = input("Please specify: ")
-                known_source_str = f"{known_sources[option - 1]}: {specification}"
-            elif option == 5:
-                specification = input("Please specify: ")
-                known_source_str = f"{known_sources[option - 1]}: {specification}"
-            else:
-                known_source_str = known_sources[option - 1]
-            return known_source_str
-        else:
-            print("Invalid option. Please choose a valid option.")
+        print("How did you hear about us?")
+        for option, known_source in known_sources.items():
+            print(f"{option}. {known_source}")
 
-def issue_escalation_options():
-    issue_escalation = {
+        option = input("Enter your option: ")
+
+        if option.strip() in known_sources:
+            if option.strip() in ['4', '5']:
+                source_specification = input("Please specify: ")
+                return known_sources[option.strip()] + " : " + source_specification
+            else:
+                return known_sources[option.strip()]
+        else:
+            print("Please choose a valid option.")
+            
+def issue_escalation():
+    issue_escalation_options = {
         '1': 'Team Lead',
         '2': 'Sales Person',
         '3': 'Escalate Issue'
     }
-    valid_options = issue_escalation.keys()
+    
     while True:
         print("Who would you like to speak to?")
-        for option, person in issue_escalation.items():
+        for option, person in issue_escalation_options.items():
             print(f"{option}. {person}")
+            
         option = input("Your choice: ")
-        options = [opt.strip() for opt in option.split(',') if opt.strip() in valid_options]
-        if options:
-            return [issue_escalation[opt] for opt in options]
+        
+        if option.strip() in issue_escalation_options:
+            return issue_escalation_options[option.strip()]
         else:
             print("Please choose valid option(s)")
-
+            
 def issue_type():
     while True:
         print("Is your issue normal or urgent?")
@@ -162,13 +179,13 @@ def issue_type():
         response = input("Enter '1' for normal or '2' for urgent: ")
         if response == "1":
             print("Thank you. We have saved your issue and will contact you as soon as possible.")
-            return ["normal"]  # Return as a list
+            return "normal"  # Return as a string
         elif response == "2":
             print("Thank you. We have saved your issue as urgent and will contact you immediately.")
-            return ["urgent"]  # Return as a list
+            return "urgent"  # Return as a string
         else:
-            print("Sorry, I didn't understand your response. Please try again.")
-
+            print("Please choose a valid option.")
+            
 def category():
     while True:
         user_type = input("Are you an experienced or fresher? (Enter 1 for experienced, 2 for fresher): ")
@@ -194,7 +211,7 @@ def interview_available_check():
             return interview_available
         else:
             print("Invalid input. Please select a valid option.")
-
+            
 def date_of_interview():
     while True:
         try:
@@ -206,39 +223,40 @@ def date_of_interview():
                 return time_avail
         except ValueError:
             print("Invalid date format. Please enter the date in dd/mm/yyyy format.")
-
+            
 def notice_period():
     while True:
         joining_date = input("When can you join? (1 for 30 days/2 for 60 days/3 for 90 days): ")
         if joining_date == '1':
-            joining_date = 30
+            joining_date = '30 days'
             return joining_date
         elif joining_date == '2':
-            joining_date = 60
+            joining_date = '60 days'
             return joining_date
         elif joining_date == '3':
-            joining_date = 90
+            joining_date = '90 days'
             return joining_date
         else:
             print("Invalid input. Please select a valid option.")
+            
+ip_location = get_location()
+weather_info = get_weather(ip_location)
+weather_info_greet = get_weather_greeting(weather_info)
 
+greeting = "Hello, buddy! Welcome to Datanetiix! We hope you're connecting from"
+print(greeting, ip_location + " " + weather_info_greet)
 
+            
 client_type = input("Can you please let me know if you are a? (Enter 1 for new client, 2 for existing client, 3 for job seeker): ")
 
 if client_type == '1':
     print("Welcome, New client!")
     try:
         name, email, contact = get_user_details()
-        print('Thanks for your details')
-        industry_options = choose_industry_options()
+        indutry_options = choose_industries_option()
         vertical_options = choose_vertical_option()
-        requirement_options = choose_requirements()
+        requirement_option = choose_requirement()
         known_source = choose_known_source()
-        
-        # Insert new client into the database
-        insert_new_client(name, email, contact, industry_options, vertical_options, requirement_options, known_source)
-
-        
     except Exception as e:
         print(str(e))
 
@@ -246,36 +264,23 @@ elif client_type == '2':
     print("Welcome, existing client!")
     try:
         name, email, contact = get_user_details()
-        print('Thanks for updating your details')
         vertical_options = choose_vertical_option()
-        issue_escalation = issue_escalation_options()
-        print("Thank you for choosing option(s).")
-        #print("Selected option(s):", ", ".join(selected_issue_escalation))
-        issue_option = issue_type()
-        
-        # Insert existing client into the database
-        insert_existing_client(name, email, contact, vertical_options, issue_escalation , issue_option)
-        
+        issue_escalation = issue_escalation()
+        issue_type = issue_type()
     except Exception as e:
         print(str(e))
-
+        
 elif client_type == '3':
     print("Welcome, Job seeker!")
     try:
         name, email, contact = get_user_details()
-        print('Thanks for providing your details')
         user_category = category()
-        print(f"You selected {user_category} category.")
-        vertical_options =choose_vertical_option()
-        interview_avail =interview_available_check()
-        time_avail=date_of_interview()
-        joining_date=notice_period()
-        
-        # Insert existing client into the database
-        insert_job_seeker(name, email, contact,user_category, vertical_options, interview_avail, time_avail, joining_date)
-        
+        is_available = interview_available_check()
+        interview_date = date_of_interview()
+        joining_date =notice_period()
     except Exception as e:
         print(str(e))
-
+                
 else:
     print("Invalid option. Please choose a valid option.")
+
